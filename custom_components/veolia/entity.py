ae248b568023e6d087d276cb169c55cb213d4376
Vincent Meurisse <github.com-c41@meurisse.org>
2022-11-27 18:27:35 +0000

    configure sensors for energy dashboard

--- custom_components/veolia/entity.py
+++ custom_components/veolia/entity.py
@@ -1,11 +1,16 @@
 """VeoliaEntity class."""
 from homeassistant.const import VOLUME_LITERS
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorEntityDescription,
+    SensorStateClass,
+)
 from .const import DOMAIN, LAST_REPORT_TIMESTAMP, NAME
 
 
-class VeoliaEntity(CoordinatorEntity):
+class VeoliaEntity(CoordinatorEntity, SensorEntity):
     """Representation of a Veolia entity."""
 
     def __init__(self, coordinator, config_entry):
@@ -13,6 +18,11 @@ class VeoliaEntity(CoordinatorEntity):
         super().__init__(coordinator)
         self.config_entry = config_entry
         self._attr_unique_id = f"{self.config_entry.entry_id}_{self.name}"
+        self.entity_description = SensorEntityDescription(
+            key=self._attr_unique_id,
+            device_class=SensorDeviceClass.WATER,
+            state_class=SensorStateClass.TOTAL,
+        )
         self._attr_unit_of_measurement = VOLUME_LITERS
         self._attr_icon = "mdi:water"
 
@@ -22,7 +32,7 @@ class VeoliaEntity(CoordinatorEntity):
         return {
             "identifiers": {(self.config_entry.entry_id, DOMAIN)},
             "manufacturer": NAME,
-            "name": NAME,
+            "name": f"{NAME} {self.config_entry.entry_id}",
         }
 
     @property
